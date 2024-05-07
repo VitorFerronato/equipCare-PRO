e3c34a79fb76b615b346a6d945728bf3a6297b89
Vitor Eduardo <vitor.eduardo@predify.me>
2024-05-07 18:13:53 -0300

    feat: first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <Header />
+  </div>
 </template>
 
+<script>
+import Header from "./components/header-app.vue";
+export default {
+  components: { Header },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.dsg-flex-center{
+  display: flex;
+  align-items: center;
 }
 </style>
---
+++ src/components/header-app.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="header dsg-flex-center">
+    <div class="">
+      <h4>Equip Care PRO</h4>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../design/_colors.scss";
+@import "../design/_sizes.scss";
+.header {
+  background-color: #ccc;
+  height: $header-height-lg;
+  @media (max-width: 1380px) {
+    height: $header-height-md;
+    background-color: red;
+  }
+}
+</style>
